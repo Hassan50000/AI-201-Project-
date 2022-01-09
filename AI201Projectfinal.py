fdbc4a241d218ff8c799265aaa2d38aeeead8443
ATIII <ahtashamahsan988@gmail.com>
2022-01-09 21:18:04 +0500

    added examples

--- AI201Projectfinal.py
+++ AI201Projectfinal.py
@@ -17,39 +17,36 @@ with sr.Microphone() as source:
         print("YOU HAVE SPOKEN THE TEXT  :: {}".format(text))
     except:
         print("Sorry we could not recognize what you just said speak again")
-        
+
     sound = "recorded.wav"
     r = sr.Recognizer()
-   
-   
+
     with sr.AudioFile(sound) as source:
           r.adjust_for_ambient_noise(source)
           print("Converting Audio To Text : ")
           audio = r.listen(source)
-   
+
     try:
           print("Your Converted Audio Is : \n" + r.recognize_google(audio))
-   
-   
+
     except Exception as e:
-          print("Error {} : ".format(e) )
-            
+          print("Error {} : ".format(e))
+
 '''If you receive errors such as No module named win32com.client, No module
 named win32, or No module named win32api, you will need to additionally install pypiwin32.
- pip install pyttsx3'''          
-   
+ pip install pyttsx3'''
+
     engine = pyttsx3.init()
 
     engine.say("welcome to MAP of GIKI... ")
     engine.setProperty('rate', 150)
     engine.setProperty('volume', 1.5)
     engine.runAndWait()
-    
-   
+
     tts = gTTS(text="  TRAVELLING WITH US... ", lang='en')
     tts.save("audiofile.mp3")
     print("Your text has been converted  .. congratulations ")
-    
+
     r = sr.Recognizer()
 
     with sr.Microphone() as source:
@@ -63,41 +60,41 @@ named win32, or No module named win32api, you will need to additionally install 
             print("You have said \n" + r.recognize_google(audio))
             print("Audio Recorded Successfully \n ")
 
-
         except Exception as e:
             print("Error :  " + str(e))
 
     with open("fileaudio.wav", "wb") as f:
-            f.write(audio.get_wav_data())  
-            
-          
+            f.write(audio.get_wav_data())
+
+
 # Implementing basic bffs andd dfs through random map as an example andd testing
 
 Graph = {
-    'S':('A','D'),
-    'A':('B','C'),
-    'D':('B','E'),
-    'B':('C','E'),
-    'E':('G'),
-    'C':('G')
+    'S': ('A', 'D'),
+    'A': ('B', 'C'),
+    'D': ('B', 'E'),
+    'B': ('C', 'E'),
+    'E': ('G'),
+    'C': ('G')
 }
 
 print(Graph)
 
 Cost = {
-    ('S','A'):3,
-    ('S','D'):2,
-    ('A','B'):5,
-    ('A','C'):10,
-    ('B','C'):2,
-    ('B','E'):1,
-    ('D','E'):4,
-    ('D','B'):1,
-    ('C','G'):4,
-    ('E','G'):3
+    ('S', 'A'): 3,
+    ('S', 'D'): 2,
+    ('A', 'B'): 5,
+    ('A', 'C'): 10,
+    ('B', 'C'): 2,
+    ('B', 'E'): 1,
+    ('D', 'E'): 4,
+    ('D', 'B'): 1,
+    ('C', 'G'): 4,
+    ('E', 'G'): 3
 }
 
 print(Cost)
+
 
 def dfs_paths(Graph, start, goal):
     stack = [(start, [start])]
@@ -108,28 +105,27 @@ def dfs_paths(Graph, start, goal):
                 yield path + [next]
             else:
                 stack.append((next, path + [next]))
-                
-print(list(dfs_paths(Graph, 'S','G')))
 
+
+print(list(dfs_paths(Graph, 'S', 'G')))
 
 
 print("The individual / possible paths of Depth first Search from start node to goal are : \n ")
 
 paths_dfs = list(dfs_paths(Graph, 'S', 'G'))
 for i in paths_dfs:
-    temp=0
-    
+    temp = 0
+
     print(i)
     print(end="  Cost is : ")
 
     for j in range(len(i)-1):
-        temp=temp + Cost[(i[j],i[j+1])]
+        temp = temp + Cost[(i[j], i[j+1])]
 
     print(temp)
     print("\n")
-    
-    
-    
+
+
 def bfs_paths(graph, start, goal):
     stack = [(start, [start])]
     while stack:
@@ -139,10 +135,9 @@ def bfs_paths(graph, start, goal):
                 yield path + [next]
             else:
                 stack.append((next, path + [next]))
-                
-                
-                
-print(list(bfs_paths(Graph, 'S','G')))
+
+
+print(list(bfs_paths(Graph, 'S', 'G')))
 
 print("The individual / possible paths of Breadth first Search from start node to goal are : \n ")
 
@@ -150,74 +145,76 @@ paths_bfs = list(bfs_paths(Graph, 'S', 'G'))
 
 
 for i in paths_bfs:
-    temp=0
-    
+    temp = 0
+
     print(i)
     print(end="  Cost is : ")
 
     for j in range(len(i)-1):
-        temp=temp + Cost[(i[j],i[j+1])]
+        temp = temp + Cost[(i[j], i[j+1])]
 
     print(temp)
     print("\n")
-    
-print("Expanded nodes are : ",dfs_expended(Gp, 'A', 'G'))     
 
-print("Expanded nodes are : ",bfs_expended(Gp, 'A', 'G'))   
+print("Expanded nodes are : ", dfs_expended(Gp, 'A', 'G'))
+
+print("Expanded nodes are : ", bfs_expended(Gp, 'A', 'G'))
 
 print("The path using breadth first search is : ")
 
-y=list(dfs_paths(Gp, 'F', 'G'))
+y = list(dfs_paths(Gp, 'F', 'G'))
 
 print(y)
 
-# FINDING Cost of possible paths 
+# FINDING Cost of possible paths
 
-def ucs_path(graph,cost,start,goal):
+
+def ucs_path(graph, cost, start, goal):
     queue = []
-    solution_cost=[]
+    solution_cost = []
     queue.append([0, start])
- 
+
     visited = {}
- 
+
     while (len(queue) > 0):
         queue = sorted(queue)
         p = queue[-1]
         del queue[-1]
-        p[0] =p[0]*-1
- 
+        p[0] = p[0]*-1
+
         if (p[1] == goal):
-            visited[p[1]]=[k for k,v in graph.items() if p[1] in v]
-            solution_cost=p[0]
+            visited[p[1]] = [k for k, v in graph.items() if p[1] in v]
+            solution_cost = p[0]
             break
         queue = sorted(queue)
- 
+
         if (p[1] not in visited):
             for i in range(len(graph[p[1]])):
                 if graph[p[1]][i] not in visited:
-                    queue.append( [(p[0] + cost[(p[1], graph[p[1]][i])])* -1, graph[p[1]][i]])
-                    path=[k for k,v in graph.items() if p[1] in v]
+                    queue.append(
+                        [(p[0] + cost[(p[1], graph[p[1]][i])]) * -1, graph[p[1]][i]])
+                    path = [k for k, v in graph.items() if p[1] in v]
         visited[p[1]] = path
-    solution = set([v[0] for v in visited.values() if len(v)>0]) | set('G')
+    solution = set([v[0] for v in visited.values() if len(v) > 0]) | set('G')
     return solution, solution_cost
 
 
+print(ucs_path(Graph, Cost, 'S', 'G'))
+print("PATH WITH ITS COST: ", ucs_path(graph, Hurestics, 'S', 'G'))
 
-print(ucs_path(Graph,Cost,'S','G'))
-print("PATH WITH ITS COST: ", ucs_path(graph,Hurestics,'S','G'))
 
-    
-def ucs(graph, start,goal):
-     visited, pqueue = [], [(0,start)]
+def ucs(graph, start, goal):
+     visited, pqueue = [], [(0, start)]
      while pqueue:
          vertex = pqueue.pop(0)
          if vertex[1] not in visited:
                visited.append(vertex[1])
-               temp=[l[0] for l in graph[vertex[1]] if l[0] not in visited]
-               cost=[l[1]+vertex[0] for l in graph[vertex[1]] if l[0] not in visited]
-               pqueue.extend(zip(cost,temp))
+               temp = [l[0] for l in graph[vertex[1]] if l[0] not in visited]
+               cost = [l[1]+vertex[0]
+                   for l in graph[vertex[1]] if l[0] not in visited]
+               pqueue.extend(zip(cost, temp))
                pqueue.sort()
- 
+
         if vertex[1]==goal:
         break
     return visited
@@ -288,12 +285,12 @@ if 'video' in r1.recognize_google(audio):
 ''' file handlin code data retrieval thu voice commaands returning specific words ..... '''
 
 
- #In text file it is printed : 
- #Hello I wanna travel from Hostel9 to Tuc
- #Command i.e. the above line is given through audio from microphone
- #The program saves that audio in a text file and then returns only our source and destination 
+ # In text file it is printed : 
+ # Hello I wanna travel from Hostel9 to Tuc
+ # Command i.e. the above line is given through audio from microphone
+ # The program saves that audio in a text file and then returns only our source and destination 
 
- #According to my program the AI smart machine would itself automatically detect the source and destination
+ # According to my program the AI smart machine would itself automatically detect the source and destination
 
  # AI Assistant voice would return something like this : 
  # Your Source is Hostel9 and your destination is Tuc Thankyou for travelling with us 
@@ -325,7 +322,7 @@ for i in range(len(sentenceList)):
 for i in afterIs:
     print(i)
 
-#********************************************************************
+# ********************************************************************
 
 myFile = open('input.txt')
 sentenceList1 = []
@@ -346,8 +343,23 @@ for i in afterIs1:
     print(i)
 
 ''' FURTHER CODE ..... '''
-
-
+Hostel_9_to_tuc = [(34.068611, 72.640354),
+                    (34.068138, 72.643522),
+                    (34.068576, 72.645982), 
+                    (34.070269, 72.647414) 
+                    ]
+SportComplex_to_hostel_11 = [
+                    (34.067887, 72.639636),
+                    (34.069456, 72.641095),
+                    (34.07101128044904, 72.64084684626256)
+]
+Hostel_6_to_MC = [
+                    (34.070220, 72.639359),
+                    (34.070389, 72.641103),
+                    (34.071613, 72.645029),
+                    (34.071701, 72.645716),
+                    (34.071411, 72.646636)
+]
 # import folium package
 import folium as f
 from folium import plugins
@@ -392,6 +404,6 @@ f.Marker(location=[34.065350, 72.660956], icon=f.Icon(color="red", icon="cloud")
 f.Marker(location=[34.071613, 72.645029], icon=f.Icon(color="black", icon="cloud"),popup="Brabers", tooltip="Brabers").add_to(my_map)
 f.Marker(location=[34.071701, 72.645716], icon=f.Icon(color="orange", icon="cloud"),popup="Incubator", tooltip="Incubator").add_to(my_map)
 
-#f.Marker(location=[],icon=f.Icon(color="orange", icon="cloud"), popup=" Clock Tower", tooltip="C TYPE").add_to(my_map)
+# f.Marker(location=[],icon=f.Icon(color="orange", icon="cloud"), popup=" Clock Tower", tooltip="C TYPE").add_to(my_map)
 
 my_map
